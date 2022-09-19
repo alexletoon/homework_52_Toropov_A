4cdb1a1ad1285f99b44b61c17b9e1e5d72fd7f5d
Alexandr Toropov <bligs100@gmail.com>
2022-09-20 02:10:30 +0600

    создана модель Task
    Совершена миграция модели task

--- source/list_app/models.py
+++ source/list_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    description = models.CharField(verbose_name='Задача', max_length=3000, null = False, blank=False)
+    status = models.CharField(verbose_name='Статус', max_length=12)
+    deadline_date = models.DateField(verbose_name='Срок выполнения', blank = True, null = False)
+    created_at = models.DateField(verbose_name='Дата создания', auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f'Task: {self.description}, Status: {self.status}, deadline: {self.deadline_date}'
