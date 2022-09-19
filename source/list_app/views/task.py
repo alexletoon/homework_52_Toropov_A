11ef2de750fd9dae74a9ffb8dab4fd6253f939ce
Alexandr Toropov <bligs100@gmail.com>
2022-09-20 01:25:32 +0600

    Создан html-файл для отображения подробной информации о задаче - task.html
    В add_task_view добавлен контекст для передачи в представление task.html

--- source/list_app/views/task.py
+++ source/list_app/views/task.py
@@ -1,7 +1,13 @@
+import re
 from django.shortcuts import render
 
 
 def add_task_view(request):
     if request.method =='GET':
         return render(request, 'new_task.html')
-    return render (request, 'index.html')
+    context = {
+        'description': request.POST.get('description'),
+        'status': request.POST.get('status'),
+        'date': request.POST.get('date')
+    }
+    return render (request, 'task.html', context=context)
